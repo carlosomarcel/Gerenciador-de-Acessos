10dc771f31c14235dddd855d819b308270d0b178
carlosomarcel <ca_marcel@hotmail.com>
2023-02-12 06:01:47 -0300

    Configuração a cryptografia com metodos encode e decoder

--- controllers/UsuarioController.js
+++ controllers/UsuarioController.js
@@ -1,13 +1,14 @@
 import models from "../models";
-import bcrypt from 'bcryptjs';
+import bcrypt from "bcryptjs";
+import token from "../services/token";
 
 // --------------------------- Registrando no Banco------------------------
 
 export default {
   add: async (req, res, next) => {
     try {
-        // criptografando a senha
-      req.body.password = await bcrypt.hash(req.body.password,10);
+      // criptografando a senha
+      req.body.password = await bcrypt.hash(req.body.password, 10);
       const reg = await models.Usuario.create(req.body);
       res.status(200).json(reg);
     } catch (e) {
@@ -18,18 +19,18 @@ export default {
     }
   },
 
-// --------------------------- Busca por ID ------------------------
+  // --------------------------- Busca por ID ------------------------
 
   query: async (req, res, next) => {
     try {
-        const reg = await models.Usuario.findOne({_id:req.query._id});
-        if (!reg){
-            res.status(404).send({
-                message:"Registro não existe"
-            });
-        }else{
-            res.status(200).json(reg);
-        }
+      const reg = await models.Usuario.findOne({ _id: req.query._id });
+      if (!reg) {
+        res.status(404).send({
+          message: "Registro não existe",
+        });
+      } else {
+        res.status(200).json(reg);
+      }
     } catch (e) {
       res.status(500).send({
         message: "Ocorreu um erro",
@@ -38,16 +39,22 @@ export default {
     }
   },
 
-// --------------------------- Listando dados -------------------------------
-
+  // --------------------------- Listando dados -------------------------------
 
   list: async (req, res, next) => {
     try {
       let valor = req.query.valor;
       // filtrando a busca
-        const reg = await models.Usuario.find({$or:[{'nome':new RegExp(valor,'i')},{'email':new RegExp(valor,'i')}]},{criacao:0})
-        .sort({'criacao':-1});       
-            res.status(200).send(reg);
+      const reg = await models.Usuario.find(
+        {
+          $or: [
+            { nome: new RegExp(valor, "i") },
+            { email: new RegExp(valor, "i") },
+          ],
+        },
+        { criacao: 0 }
+      ).sort({ criacao: -1 });
+      res.status(200).send(reg);
     } catch (e) {
       res.status(500).send({
         message: "Ocorreu um erro",
@@ -56,22 +63,33 @@ export default {
     }
   },
 
-// --------------------------- Atualizando dados ------------------------
+  // --------------------------- Atualizando dados ------------------------
 
   update: async (req, res, next) => {
     try {
       // criptografando o updade da senha
       let pas = req.body.password;
 
-      const reg0 = await models.Usuario.findOne({_id:req.body._id});
+      const reg0 = await models.Usuario.findOne({ _id: req.body._id });
 
-      if(pas!=reg0.password){      
-        req.body.password = await bcrypt.hash(req.body.password,10);
+      if (pas != reg0.password) {
+        req.body.password = await bcrypt.hash(req.body.password, 10);
       }
-     
-        const reg = await models.Usuario.findByIdAndUpdate({_id:req.body._id},{role:req.body.role,nome:req.body.nome, tipo_documento:req.body.tipo_documento,
-           num_documento:req.body.num_documento,direcao:req.body.direcao, telefone:req.body.telefone, email:req.body.email, password:req.body.password});
-        res.status(200).json(reg);
+
+      const reg = await models.Usuario.findByIdAndUpdate(
+        { _id: req.body._id },
+        {
+          role: req.body.role,
+          nome: req.body.nome,
+          tipo_documento: req.body.tipo_documento,
+          num_documento: req.body.num_documento,
+          direcao: req.body.direcao,
+          telefone: req.body.telefone,
+          email: req.body.email,
+          password: req.body.password,
+        }
+      );
+      res.status(200).json(reg);
     } catch (e) {
       res.status(500).send({
         message: "Ocorreu um erro",
@@ -80,12 +98,12 @@ export default {
     }
   },
 
- // --------------------------- Deletando dados ------------------------ 
+  // --------------------------- Deletando dados ------------------------
 
   remove: async (req, res, next) => {
     try {
-        const reg = await models.Usuario.findOneAndDelete({_id:req.body._id});
-        res.status(200).json(reg);
+      const reg = await models.Usuario.findOneAndDelete({ _id: req.body._id });
+      res.status(200).json(reg);
     } catch (e) {
       res.status(500).send({
         message: "Ocorreu um erro",
@@ -94,61 +112,66 @@ export default {
     }
   },
 
-// --------------------------- Ativos  ------------------------
+  // --------------------------- Ativos  ------------------------
 
-activate: async (req,res,next) => {
-  try {
-      const reg = await models.Usuario.findByIdAndUpdate({_id:req.body._id},{estado:1});
-      res.status(200).json(reg);
-  } catch(e){
-      res.status(500).send({
-          message:'Ocorreu um erro'
-      });
-      next(e);
-  }
-},
-
-// --------------------------- Desativados ------------------------
-
-deactivate:async (req,res,next) => {
-  try {
-      const reg = await models.Usuario.findByIdAndUpdate({_id:req.body._id},{estado:0});
-      res.status(200).json(reg);
-  } catch(e){
-      res.status(500).send({
-          message:'Ocorreu um erro'
-      });
-      next(e);
-  }
-},
-login: async (req,res,next) =>{
+  activate: async (req, res, next) => {
     try {
-        let user = await models.Usuario.findOne({email:req.body.email});
+      const reg = await models.Usuario.findByIdAndUpdate(
+        { _id: req.body._id },
+        { estado: 1 }
+      );
+      res.status(200).json(reg);
+    } catch (e) {
+      res.status(500).send({
+        message: "Ocorreu um erro",
+      });
+      next(e);
+    }
+  },
 
-        if (user){
-          //existe um usuario com esse email
-          let match = await bcrypt.compare(req.body.password, user.password);
-          if (match){
-            res.json('Senha correta');
-          }else{
-            res.status(404).send({
-              message: 'Senha incorreta'
-            });
+  // --------------------------- Desativados ------------------------
 
-          }
+  deactivate: async (req, res, next) => {
+    try {
+      const reg = await models.Usuario.findByIdAndUpdate(
+        { _id: req.body._id },
+        { estado: 0 }
+      );
+      res.status(200).json(reg);
+    } catch (e) {
+      res.status(500).send({
+        message: "Ocorreu um erro",
+      });
+      next(e);
+    }
+  },
+   // --------------------------- Login ------------------------
+  login: async (req, res, next) => {
+    try {
+      let user = await models.Usuario.findOne({ email: req.body.email, estado:1});
 
-        }else{
+      if (user) {
+        //existe um usuario com esse email
+        let match = await bcrypt.compare(req.body.password, user.password);
+        if (match) {
+          let tokenReturn = await token.encode(user._id);
+          res.status(200).json({user,tokenReturn})
+        
+        } else {
           res.status(404).send({
-            message: 'Não existe o usuário'
+            message: "Senha incorreta",
           });
         }
-
-
-    } catch(e){
+      } else {
+        res.status(404).send({
+          message: "Não existe o usuário",
+        });
+      }
+    } catch (e) {
       res.status(500).send({
-          message:'Ocorreu um erro'
+        message: "Ocorreu um erro",
       });
-
+      next(e)
     }
-}
-}
+  },
+};
