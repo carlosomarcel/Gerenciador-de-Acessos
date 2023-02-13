7830e8a1b5ef75aec31541bea0c6aeedc04ceb10
carlosomarcel <ca_marcel@hotmail.com>
2023-02-13 16:48:59 -0300

    Inserindo a rota pessoas

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,14 @@ import routerx from "express-promise-router";
 import categoriaRouter from './categoria';
 import artigoRouter from './artigo'
 import usuarioRouter from './usuario';
+import pessoaRouter from "./pessoa";
 
 const router = routerx();
 
 router.use('/categoria', categoriaRouter);
 router.use('/artigo', artigoRouter);
 router.use('/usuario', usuarioRouter)
+router.use('/pessoa', pessoaRouter)
 
 
 export default router;
--- routes/pessoa.js
+++ routes/pessoa.js
@@ -7,11 +7,13 @@ const router = routerx();
 router.post('/add',auth.verifyUsuario, pessoaController.add);
 router.get('/query',auth.verifyUsuario, pessoaController.query);
 router.get('/list',auth.verifyUsuario, pessoaController.list);
-router.put('/update',auth.verifyUsuario, usuarioController.update);
-router.delete('/remove',auth.verifyUsuario, usuarioController.remove);
-router.put('/activate',auth.verifyUsuario, usuarioController.activate);
-router.put('/deactivate',auth.verifyUsuario, usuarioController.deactivate);
-router.post('/login', usuarioController.login);
+router.get('/listClientes',auth.verifyUsuario, pessoaController.list);
+router.get('/listFornecedores',auth.verifyUsuario, pessoaController.list);
+router.put('/update',auth.verifyUsuario, pessoaController.update);
+router.delete('/remove',auth.verifyUsuario, pessoaController.remove);
+router.put('/activate',auth.verifyUsuario, pessoaController.activate);
+router.put('/deactivate',auth.verifyUsuario, pessoaController.deactivate);
+
 
 
 export default router;
